8b4881eca75df69ebb7381df2c7e1ce0b0b2eef7
Z-Dub89 <94818823+Z-Dub89@users.noreply.github.com>
2022-03-01 22:04:35 -0600

    Movement Script( Though Not Quite Finished)
    Has one part of the script completed, however Im still working on the other part, and it should be done soon.

--- GMNG4340_SeniorProject/Assets/Scripts/Movement.cs
+++ GMNG4340_SeniorProject/Assets/Scripts/Movement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string verticalAxisName = "Vertical";
+    public string horizontalAxisName = "Horizontal";
+    public string brakingKey = "Brake";
+
+
+    [HideInInspector] public float thruster;
+    [HideInInspector] public float rudder;
+    [HideInInspector] public bool isBraking;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Exit game once Escape kay is pressed.
+        if (Input.GetButtonDown("Cancel") && !Application.isEditor)
+            Application.Quit();
+        /*
+        if(GameManager.instace != null && !GameManager.instance.isActiveGame())
+        {
+
+            thruster = rudder = 0f;
+            isBraking = false;
+            return;
+
+        }
+        */
+
+        thruster = Input.GetAxis(verticalAxisName);
+        rudder = Input.GetAxis(horizontalAxisName);
+        isBraking = Input.GetButton(brakingKey);
+
+
+
+
+
+
     }
 }
