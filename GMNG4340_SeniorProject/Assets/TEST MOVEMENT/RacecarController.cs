c5cd6266754e526bd523c826f4275c9d74cf8e82
ZepicZorua <zepiczorua@gmail.com>
2022-03-29 03:05:08 -0500

    Zepic Movement
    Might need to install Input System from Package Manager for this to work.

---
+++ GMNG4340_SeniorProject/Assets/TEST MOVEMENT/RacecarController.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RacecarController : MonoBehaviour
+{
+    public InputActionReference steer;
+    public InputActionReference gas;
+    public InputActionReference drift;
+    public InputActionReference boost;
+    
+
+    public Rigidbody rb;
+    public float maxSpeed;
+    public float rotationSpeed;
+    public GameObject driftObject;
+    public float moveSpeed;
+    public float turnSpeed;
+    public float rotateTurnSpeed;
+
+    public float timer = 0.0f;
+    public float boostTimer = 5f;
+    public bool boosted;
+
+    public float _steer;
+    public bool _gas;
+    public bool _drift;
+    public bool _boost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        steer.asset.Enable();
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+
+        _steer = steer.ToInputAction().ReadValue<float>();
+        _gas = gas.ToInputAction().ReadValue<float>() > 0;
+        _drift = drift.ToInputAction().ReadValue<float>() > 0;
+        _boost = boost.ToInputAction().ReadValue<float>() > 0;
+
+        //GUILayout.Label(_steer.ToString());
+        GUILayout.Label(_gas.ToString());
+        GUILayout.Label(_drift.ToString());
+        GUILayout.Label(_boost.ToString());
+    }
+
+
+    void FixedUpdate()
+    {
+        //Drift Boost
+        if (_drift == false && timer >= 1f)
+        {
+            rb.AddForce(transform.forward * 1000);
+            timer = 0.0f;
+        }
+
+        if (_drift == false)
+        {
+            moveSpeed = 20f;
+            timer = 0.0f;
+        }
+
+
+
+        //Boost and the boost cooldown
+        if (boosted == true)
+        {
+            boostTimer -= Time.deltaTime;
+
+            if (boostTimer < 0)
+            {
+                boostTimer = 5f;
+                boosted = false;
+            }
+        }
+
+        if (_boost == true && boostTimer == 5f)
+        {
+            rb.AddForce(transform.forward * 2000);
+            boosted = true;
+        }
+
+
+
+        //This normalizes the speed when you hit the max speed
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+            Debug.Log("Hit max speed");
+        }
+
+
+        //Gas
+        if (_gas == true)
+        {
+            rb.AddForce(transform.forward * moveSpeed);
+        }
+
+
+
+        //Drifting
+        if (_steer < 0 && _drift == true)
+        {
+            timer += Time.deltaTime;
+            moveSpeed = 7f;
+            this.transform.RotateAround(driftObject.transform.position, -Vector3.up, rotationSpeed);
+        }
+        if (_steer > 0 && _drift == true)
+        {
+            timer += Time.deltaTime;
+            moveSpeed = 7f;
+            this.transform.RotateAround(driftObject.transform.position, Vector3.up, rotationSpeed);
+        }
+
+
+
+        //Turning to the left or right
+        if (_steer < 0 && _drift == false)
+        {
+            this.transform.Translate(Vector3.left * turnSpeed * Time.deltaTime, Space.Self);
+            this.transform.RotateAround(driftObject.transform.position, -Vector3.up, rotateTurnSpeed);
+        }
+        if (_steer > 0 && _drift == false)
+        {
+            this.transform.Translate(Vector3.right * turnSpeed * Time.deltaTime, Space.Self);
+            this.transform.RotateAround(driftObject.transform.position, Vector3.up, rotateTurnSpeed);
+        }
+    }
+}
---
+++ GMNG4340_SeniorProject/Assets/TEST MOVEMENT/Test/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public TestControls playerInput;
+    public float maxSpeed;
+    public float rotationSpeed;
+
+    public GameObject driftObject;
+
+
+    public Vector2 i_movement;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = new TestControls();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+            Debug.Log("Hit max speed");
+        }
+        
+    }
+
+    void Move()
+    {
+        rb.AddForce(transform.forward * moveSpeed);
+    }
+
+    void OnMove(InputValue value)
+    {
+        i_movement = value.Get<Vector2>();
+        Move();
+    }
+
+    void OnDrift()
+    {
+        if (i_movement.x != 0)
+        {
+            Debug.Log("Can Drift!!!");
+            if (i_movement.x < 0)
+            {
+                this.transform.RotateAround(driftObject.transform.position, -Vector3.up, rotationSpeed);
+            }
+        }
+        else
+        {
+            Debug.Log("Can't Drift");
+        }
+    }
+}
