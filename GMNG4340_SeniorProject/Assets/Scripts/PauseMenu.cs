d4a849738ab19ba70f3f11af100adcda596ad28a
IvoryBells <TwillaCherry77@yahoo.com>
2022-05-07 12:23:28 -0500

    Reset Button added to Pause Menu

--- GMNG4340_SeniorProject/Assets/Scripts/PauseMenu.cs
+++ GMNG4340_SeniorProject/Assets/Scripts/PauseMenu.cs
@@ -48,4 +48,10 @@ public class PauseMenu : MonoBehaviour
         Debug.Log("Quitting game...");
         Application.Quit();
     }
+
+    public void Reset()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
